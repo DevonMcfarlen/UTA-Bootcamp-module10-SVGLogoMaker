7930eaad8f934a81a4938a5d594f7195fe4078e8
DevonMcfarlen <mcfarlendevon@gmail.com>
2023-11-13 14:36:36 -0600

    created promprs for user input

---
+++ index.js
@@ -0,0 +1,25 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateSVG = require('./utils/generateSVG');
+
+const shapes = [ "Triangle", "Circle", "Square"];
+
+const questions = [
+    { type: 'list', message: 'Please select a shape for the logo.', choices: shapes, name: 'userShape' },
+    { type: 'input', message: 'Please enter no more than 3 characters for your logo.', name: 'userText' },
+    { type: 'input', message: 'Please enter a color or hex code for your shape.', name: 'userShapeColor' },
+    { type: 'input', message: 'Please enter a color or hex code for your text.', name: 'userTextColor' }
+];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateSVG(data), (err) =>
+    err ? console.error(err) : console.log('Logo Created!'));
+}
+
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => writeToFile('new-logo.svg', response));
+}
+
+init();
