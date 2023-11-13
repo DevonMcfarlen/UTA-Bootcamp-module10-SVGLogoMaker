2fe2ae0687f4ac983901130aebc778dc03162883
DevonMcfarlen <mcfarlendevon@gmail.com>
2023-11-13 15:03:55 -0600

    let users set the logo filename

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const generateSVG = require('./utils/generateSVG');
 const shapes = [ "Triangle", "Circle", "Square"];
 
 const questions = [
+    {type: 'input', message: 'Please enter a name for your logo.', name: 'userLogoName' },
     { type: 'list', message: 'Please select a shape for the logo.', choices: shapes, name: 'userShape' },
     { type: 'input', message: 'Please enter no more than 3 characters for your logo.', name: 'userText' },
     { type: 'input', message: 'Please enter a color or hex code for your shape.', name: 'userShapeColor' },
@@ -19,7 +20,7 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer
         .prompt(questions)
-        .then((response) => writeToFile('new-logo.svg', response));
+        .then((response) => writeToFile(`${response.userLogoName}.svg`, response));
 }
 
 init();
